43176bffa7dbc8d120d620d680c7b114b63e8fcb
ushanil <ush.gopi@gmail.com>
2020-01-03 11:23:47 -0500

    Generics - stack, group and sorted group - done

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -1,18 +1,23 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.Collections;
+
 /**
  * @author leon on 18/12/2018.
  */
-public class SortedGroup<_> extends Group<_> {
+public class SortedGroup<_ extends Comparable> extends Group<_> {
     @Override
     public void insert(_ value) {
+        groupList.add(value);
+        Collections.sort(groupList);
     }
 
     @Override
     public void delete(_ value) {
+        groupList.remove(value);
     }
 
     public Integer indexOf(_ value) {
-        return null;
+        return groupList.indexOf(value);
     }
 }
