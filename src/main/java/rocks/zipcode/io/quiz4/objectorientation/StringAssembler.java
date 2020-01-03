6a1d6d38fbe950b62f916864fa98f7c9d6b18e39
ushanil <ush.gopi@gmail.com>
2020-01-03 10:41:41 -0500

    Collections,Fundamentals and objectOrientation - all done 111 tests passed

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,14 +4,20 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+    String finalString = "";
+    Character delimiter;
     public StringAssembler(Character delimeter) {
+        delimiter = delimeter;
+
     }
 
     public StringAssembler append(String str) {
-        return null;
+
+        finalString += str + delimiter;
+         return this;
     }
 
     public String assemble() {
-        return null;
+        return this.finalString.substring(0, finalString.length()-1);
     }
 }
